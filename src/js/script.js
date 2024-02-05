bfe3a1c98d555398da3f363edad0a5a768e65560
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-05 22:29:41 +0000

    Added classes and functions for animations

--- src/js/script.js
+++ src/js/script.js
@@ -70,6 +70,14 @@
      * Handle the scroll event and update styles for elements with the "progress" class to increase the progress bar width.
      */
     const handleScroll = () => {
+        document.querySelectorAll(".show-on-scroll").forEach((element) => {
+            if (isElementInViewport(element)) {
+                element.classList.add("is-visible");
+            } else {
+                element.classList.remove("is-visible");
+            }
+        });
+
         document.querySelectorAll(".progress").forEach((element) => {
             const progressBar = element.lastElementChild;
 
@@ -90,4 +98,32 @@
 
     // Call the function to update copyright info
     updateCopyrightInfo();
+
+    const isElementInViewport = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            (rect.top <= 0 && rect.bottom >= 0) ||
+            (rect.bottom >=
+                (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.top <=
+                    (window.innerHeight ||
+                        document.documentElement.clientHeight)) ||
+            (rect.top >= 0 &&
+                rect.bottom <=
+                    (window.innerHeight ||
+                        document.documentElement.clientHeight))
+        );
+    };
+
+    // Smooth scroll function
+    const headerBtn = document.getElementById("header-btn");
+    const contactForm = document.getElementById("contact");
+
+    const scrollToForm = () => {
+        contactForm.scrollIntoView({ behavior: "smooth" }); // Top
+    };
+
+    headerBtn.addEventListener("click", scrollToForm);
+
+    handleScroll();
 })();
