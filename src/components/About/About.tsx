5c6eb4daa5e196840ef9f770976c4e58776bf7ec
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-21 22:22:04 +0100

    Add JSDoc and change section header to 'About Me'

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,14 @@
 import PageSection from "@/components/PageSection/PageSection";
 import AboutBody from "@/components/About/AboutBody";
 
+/**
+ * About component displays information about the user
+ * @component
+ * @returns {JSX.Element} JSX Element representing the About component
+ */
 export default function About() {
     return (
-        <PageSection id="about" header="About">
+        <PageSection id="about" header="About Me">
             <AboutBody />
         </PageSection>
     );
