0333e1a6d3be4c98e3f5f946a75bca55e57c4e31
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-23 21:08:28 +0100

    Add metadata and site icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,36 @@ export const metadata: Metadata = {
     title: "Karl Horning's Portfolio Site",
     description:
         "Welcome to Karl Horning's digital domain—a curated collection of projects, insights, and innovations in web development and design.",
+    generator: "Next.js",
+    applicationName: "Karl Horning's Portfolio Site",
+    keywords: [
+        "Next.js",
+        "JavaScript",
+        "GraphQL",
+        "Software Engineer",
+        "Portfolio",
+    ],
+    authors: [
+        {
+            name: "Karl Horning",
+            url: "https://www.linkedin.com/in/karl-horning/",
+        },
+    ],
+    creator: "Karl Horning",
+    publisher: "Karl Horning",
+    robots: {
+        index: false,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: false,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
 };
 
 export default function RootLayout({
@@ -16,7 +46,11 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
+        <html
+            lang="en"
+            className="scroll-smooth"
+            style={{ scrollBehavior: "smooth" }}
+        >
             <body className={inter.className}>{children}</body>
         </html>
     );
