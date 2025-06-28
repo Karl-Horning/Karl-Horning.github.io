cd9e9530179407d2e35f8eef9bad80853d6db432
Karl Horning <k.horning@me.com>
2025-06-28 07:50:28 +0100

    Add links file

---
+++ src/app/constants/links.ts
@@ -0,0 +1,36 @@
+/**
+ * A collection of external links used throughout the site.
+ * Useful for maintaining consistent URLs for social profiles, apps, and repositories.
+ */
+export const externalLinks = {
+    /** Karl Horning's CodePen profile link */
+    codePen: "https://codepen.io/karlhorning",
+
+    /** Karl Horning's GitHub profile link  */
+    gitHub: "https://github.com/Karl-Horning",
+
+    /** Karl Horning's LinkedIn profile link */
+    linkedIn: "https://www.linkedin.com/in/karl-horning/",
+
+    /** Karl Horning's portfolio site repo */
+    portfolio: "https://github.com/Karl-Horning/Karl-Horning.github.io",
+
+    /** Learnlight iOS App link */
+    learnlight: {
+        iosApp: "https://apps.apple.com/gb/app/learnlight/id1479158492",
+
+        /** Learnlight Android App link */
+        playApp:
+            "https://play.google.com/store/apps/details?id=com.ct.learnlight",
+    },
+};
+
+/**
+ * A collection of internal route paths for use in navigation and routing.
+ */
+export const internalRoutes = {
+    home: "/",
+    about: "/about",
+    blog: "/blog",
+    contact: "/contact",
+};
