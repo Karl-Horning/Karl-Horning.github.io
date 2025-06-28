c9ee088baaa0479b8c6afd60b369ef8844c24aca
Karl Horning <k.horning@me.com>
2025-06-28 08:23:13 +0100

    Remove unneeded components

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,5 +1,4 @@
 import { FaDatabase, FaJs, FaReact, FaUniversalAccess } from "react-icons/fa6";
-import Header from "../Header";
 import SkillsCard from "./SkillsCard";
 import Section from "../Section";
 
