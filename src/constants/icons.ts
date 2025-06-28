5c780c801c68602c9a8360b6ba774e2736ae5425
Karl Horning <k.horning@me.com>
2025-06-28 11:01:41 +0100

    Add icons constants

---
+++ src/constants/icons.ts
@@ -0,0 +1,44 @@
+import {
+    FaCodepen,
+    FaDatabase,
+    FaEnvelope,
+    FaGithub,
+    FaJs,
+    FaLinkedin,
+    FaReact,
+    FaRss,
+    FaUniversalAccess,
+} from "react-icons/fa6";
+
+/**
+ * A collection of icon components from react-icons,
+ * for consistent usage across the site.
+ */
+export const icons = {
+    /** Accessibility icon  */
+    accessibility: FaUniversalAccess,
+
+    /** CodePen icon  */
+    codePen: FaCodepen,
+
+    /** Contact icon  */
+    contact: FaEnvelope,
+
+    /** Database icon  */
+    database: FaDatabase,
+
+    /** GitHub icon  */
+    gitHub: FaGithub,
+
+    /** JavaScript icon  */
+    javaScript: FaJs,
+
+    /** LinkedIn icon  */
+    linkedIn: FaLinkedin,
+
+    /** React icon  */
+    react: FaReact,
+
+    /** RSS feed  */
+    rss: FaRss,
+};
