c11b79af710d7c13158a7abc31011146c07bc438
Karl Horning <k.horning@me.com>
2025-07-22 22:34:26 +0100

    Add CMALT Guidance

--- src/app/cmalt/section-1/1c-supporting-the-deployment-of-learning-technologies/page.tsx
+++ src/app/cmalt/section-1/1c-supporting-the-deployment-of-learning-technologies/page.tsx
@@ -5,14 +5,32 @@ import { createMetadata } from "@/lib/metadata";
 const { CmaltRoute } = internalRoutes;
 
 export const metadata = createMetadata({
-    title: "Test | CMALT",
+    title: "1c: Supporting the Deployment of Learning Technologies | CMALT",
     path: CmaltRoute,
 });
 
 export default function page() {
     return (
-        <CMALTLayout title="Test" lastUpdated="2025-07-22">
+        <CMALTLayout title="1c: Supporting the Deployment of Learning Technologies" lastUpdated="2025-07-22">
             <h2>CMALT Guidance</h2>
+
+            <p>
+                Statements about your involvement in supporting the deployment
+                of Learning Technology might relate to providing technical
+                and/or pedagogic support to teachers or learners, advising on
+                (or redesigning to take account of) technical and usability
+                issues, developing strategies or policies, managing change,
+                providing training or other forms of professional development,
+                securing or deploying dedicated funding and so on, all within
+                the context of the educational use of Learning Technology.
+            </p>
+
+            <p>
+                For evidence, you might include the overview section of a
+                strategy document, meeting minutes, summaries of student
+                feedback, testimonials or witness statements from other
+                colleagues.
+            </p>
 
             <h2>Description</h2>
 
