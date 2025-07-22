36eb808ce69b7d027fdd45e0a6d7451ac1f2722f
Karl Horning <k.horning@me.com>
2025-07-22 17:50:38 +0100

    Create CMALT constants

---
+++ src/constants/cmalt.ts
@@ -0,0 +1,84 @@
+export const cmaltSections = [
+    { slug: "", label: "Overview" },
+    { slug: "contextual-statement", label: "Contextual Statement" },
+    {
+        slug: "section-1",
+        label: "Section 1: Operational Issues",
+        children: [
+            {
+                slug: "1a-an-understanding-of-the-constraints-and-benefits-of-different-technology",
+                label: "1a: Constraints and Benefits",
+            },
+            {
+                slug: "1b-technical-knowledge-and-ability-in-the-use-of-learning-technology",
+                label: "1b: Technical Knowledge and Ability",
+            },
+            {
+                slug: "1c-supporting-the-deployment-of-learning-technologies",
+                label: "1c: Deployment Support",
+            },
+        ],
+    },
+    {
+        slug: "section-2",
+        label: "Section 2: Teaching, Learning and/or Assessment Processes",
+        children: [
+            {
+                slug: "2a-an-understanding-of-teaching-learning-and-or-assessment-processes",
+                label: "2a: Teaching, Learning and/or Assessment",
+            },
+            {
+                slug: "2b-an-understanding-of-your-target-learners",
+                label: "2b: Understanding of Target Learners",
+            },
+        ],
+    },
+    {
+        slug: "section-3",
+        label: "Section 3: The Wider Context",
+        children: [
+            {
+                slug: "3a-understanding-and-engaging-with-legislation",
+                label: "3a: Engagement with Legislation",
+            },
+            {
+                slug: "3b-understanding-and-engaging-with-policies-and-standards",
+                label: "3b: Engagement with Policies and Standards",
+            },
+        ],
+    },
+    {
+        slug: "section-4",
+        label: "Section 4: Communication and Working with Others",
+        children: [
+            {
+                slug: "4a-communication-and-working-with-others",
+                label: "4a: Communication and Working with Others",
+            },
+        ],
+    },
+    {
+        slug: "section-5",
+        label: "Section 5: Specialist Option(s)",
+        children: [
+            {
+                slug: "5a-specialist-area-1",
+                label: "5a: Specialist Area: JavaScript Development",
+            },
+        ],
+    },
+    {
+        slug: "section-6",
+        label: "Section 6: Future Plans",
+        children: [
+            {
+                slug: "6a-future-plans",
+                label: "6a: Future Plans for Development",
+            },
+            {
+                slug: "6b-confirmation-and-optional-nominated-assessor",
+                label: "6b: Declaration and Nominated Assessor (optional)",
+            },
+        ],
+    },
+];
