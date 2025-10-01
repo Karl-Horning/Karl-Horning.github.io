cec92903bf12b27d28030b91d09ecd4198e8cdfb
Karl Horning <k.horning@me.com>
2025-10-02 00:33:42 +0100

    feat(homepage): update section imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Blog from "@/components/Blog";
-import Contact from "@/components/Contact";
-import Hero from "@/components/Hero";
+import Hero from "@/components/home/Hero";
+import Highlights from "@/components/home/Highlights";
+import Skills from "@/components/home/Skills";
+import Blog from "@/components/home/Blog";
+import Contact from "@/components/home/Contact";
 
 import { createMetadata } from "@/lib/metadata";
 import { internalRoutes } from "@/lib/constants/links";
+import Projects from "@/components/home/Projects";
 
 const { HomeRoute } = internalRoutes;
 
@@ -18,8 +19,9 @@ export default function Home() {
     return (
         <main>
             <Hero />
-            <About />
+            <Highlights />
             <Skills />
+            <Projects />
             <Blog />
             <Contact />
         </main>
