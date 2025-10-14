c3e22f885aaf2296504476ef1222f664a6463aed
Karl Horning <k.horning@me.com>
2025-10-14 18:40:45 +0100

    ux: add updated table styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./styles/fonts.css";
 import "./styles/globals.css";
+import "./styles/tables.css";
 import Footer from "@/components/shell/Footer";
 import { jsonLdHome } from "@/lib/constants/jsonLd";
 import Header from "@/components/shell/Header";
