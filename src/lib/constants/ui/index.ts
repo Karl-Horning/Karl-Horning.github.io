6651b31abf0c4001acf4bc0290144c9c054501f7
Karl Horning <k.horning@me.com>
2025-10-21 20:53:25 +0100

    refactor: divide UI constants

---
+++ src/lib/constants/ui/index.ts
@@ -0,0 +1,6 @@
+export * from "./assets";
+export * from "./external.links";
+export * from "./icons";
+export * from "./internal.routes";
+export * from "./nav.links";
+export * from "./social.links";
