729c4f53a60bd4b3b01a7358526aa11b9ba005dd
Karl Horning <k.horning@me.com>
2026-04-23 07:37:07 +0100

    feat: add CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import CtaStrip from "@/components/CtaStrip";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero />
             <About />
             <Projects />
+            <CtaStrip />
         </>
     );
 }
