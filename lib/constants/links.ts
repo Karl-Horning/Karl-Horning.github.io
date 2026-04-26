92739fd1042684fab9d711685048b472516a8318
Karl Horning <k.horning@me.com>
2026-04-26 16:01:03 +0100

    chore:update GitHub links

--- lib/constants/links.ts
+++ lib/constants/links.ts
@@ -2,7 +2,7 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/karl-horning/";
 
 /** Karl Horning's GitHub profile URL. */
-export const GITHUB_URL = "https://github.com/Karl-Horning";
+export const GITHUB_URL = "https://github.com/karl-horning";
 
 /** Karl Horning's CodePen profile URL. */
 export const CODEPEN_URL = "https://codepen.io/karlhorning";
@@ -15,15 +15,15 @@ export const RSS_FEED_URL = "https://www.karlhorning.dev/rss.xml";
 
 /** Repo: Oxford Learner's Dictionaries API PoC */
 export const OLD_API_POC_URL =
-    "https://github.com/Karl-Horning/oxford-learners-dictionaries-api";
+    "https://github.com/karl-horning/oxford-learners-dictionaries-api";
 
 /** Repo: GraphQL Rate Limit Demo PoC */
 export const RATE_LIMIT_POC_URL =
-    "https://github.com/Karl-Horning/graphql-rate-limit-demo";
+    "https://github.com/karl-horning/graphql-rate-limit-demo";
 
 /** Repo: Color Contrast Checker */
 export const CCC_REPO_URL =
-    "https://github.com/Karl-Horning/color-contrast-checker-pwa";
+    "https://github.com/karl-horning/color-contrast-checker-pwa";
 
 /** Live: Color Contrast Checker */
 export const CCC_DEMO_URL =
@@ -43,18 +43,17 @@ export const FIREFOX_STORE_URL =
 
 /** Repo: Transform Text */
 export const TRANSFORM_TEXT_REPO_URL =
-    "https://github.com/Karl-Horning/transform-text-extension";
+    "https://github.com/karl-horning/transform-text-extension";
 
 /** Live: Transform Text */
 export const TRANSFORM_TEXT_URL = "https://www.karlhorning.dev/transform-text/";
 
 /** Repo: Canvas Content Styling Guide */
-export const CANVAS_GUIDE_REPO_URL =
-    "https://github.com/Karl-Horning/canvas";
+export const CANVAS_GUIDE_REPO_URL = "https://github.com/karl-horning/canvas";
 
 /** Live: Canvas Content Styling Guide */
 export const CANVAS_GUIDE_URL = "https://canvas.karlhorning.dev";
 
 /** Repo: karlhorning.dev */
 export const KARLHORNING_DEV_REPO_URL =
-    "https://github.com/Karl-Horning/karlhorning.dev";
+    "https://github.com/Karl-Horning/karl-horning.github.io";
